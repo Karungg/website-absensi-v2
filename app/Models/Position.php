63a044a8854a169a2b9aa30174be1c4bf68bee58
Karungg <miftahfadilah71@gmail.com>
2024-10-21 06:44:05 +0700

    Test

--- app/Models/Position.php
+++ app/Models/Position.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Position extends Model
 {
-    use HasFactory, HasUuids;
+    use HasFactory;
 
     public $incrementing = false;
     protected $keyType = 'string';
